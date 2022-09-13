b8545ce38a0c5eb5d01b23d64b006a07815effe2
nmer <2309382194@qq.com>
2022-09-13 20:52:43 +0800

    event2.0

--- dist/index.fef1cfaf.js
+++ dist/index.fef1cfaf.js
@@ -463,103 +463,33 @@ const level5 = document.querySelector(".level5");
 const level6 = document.querySelector(".level6");
 const level7 = document.querySelector(".level7");
 let n = 1;
-level1.addEventListener("click", (e)=>{
+let fn = (e)=>{
     const t = e.currentTarget;
     setTimeout(()=>{
         t.classList.remove("x");
     }, n * 500);
     n++;
-}, true);
-level1.addEventListener("click", (e)=>{
+};
+let fm = (e)=>{
     const t = e.currentTarget;
     setTimeout(()=>{
         t.classList.add("x");
     }, n * 500);
     n++;
-});
-level2.addEventListener("click", (e)=>{
-    const t = e.currentTarget;
-    setTimeout(()=>{
-        t.classList.remove("x");
-    }, n * 500);
-    n++;
-}, true);
-level2.addEventListener("click", (e)=>{
-    const t = e.currentTarget;
-    setTimeout(()=>{
-        t.classList.add("x");
-    }, n * 500);
-    n++;
-});
-level3.addEventListener("click", (e)=>{
-    const t = e.currentTarget;
-    setTimeout(()=>{
-        t.classList.remove("x");
-    }, n * 500);
-    n++;
-}, true);
-level3.addEventListener("click", (e)=>{
-    const t = e.currentTarget;
-    setTimeout(()=>{
-        t.classList.add("x");
-    }, n * 500);
-    n++;
-});
-level4.addEventListener("click", (e)=>{
-    const t = e.currentTarget;
-    setTimeout(()=>{
-        t.classList.remove("x");
-    }, n * 500);
-    n++;
-}, true);
-level4.addEventListener("click", (e)=>{
-    const t = e.currentTarget;
-    setTimeout(()=>{
-        t.classList.add("x");
-    }, n * 500);
-    n++;
-});
-level5.addEventListener("click", (e)=>{
-    const t = e.currentTarget;
-    setTimeout(()=>{
-        t.classList.remove("x");
-    }, n * 500);
-    n++;
-}, true);
-level5.addEventListener("click", (e)=>{
-    const t = e.currentTarget;
-    setTimeout(()=>{
-        t.classList.add("x");
-    }, n * 500);
-    n++;
-});
-level6.addEventListener("click", (e)=>{
-    const t = e.currentTarget;
-    setTimeout(()=>{
-        t.classList.remove("x");
-    }, n * 500);
-    n++;
-}, true);
-level6.addEventListener("click", (e)=>{
-    const t = e.currentTarget;
-    setTimeout(()=>{
-        t.classList.add("x");
-    }, n * 500);
-    n++;
-});
-level7.addEventListener("click", (e)=>{
-    const t = e.currentTarget;
-    setTimeout(()=>{
-        t.classList.remove("x");
-    }, n * 500);
-    n++;
-}, true);
-level7.addEventListener("click", (e)=>{
-    const t = e.currentTarget;
-    setTimeout(()=>{
-        t.classList.add("x");
-    }, n * 500);
-    n++;
-});
+};
+level1.addEventListener("click", fn, true);
+level1.addEventListener("click", fm);
+level2.addEventListener("click", fn, true);
+level2.addEventListener("click", fm);
+level3.addEventListener("click", fn, true);
+level3.addEventListener("click", fm);
+level4.addEventListener("click", fn, true);
+level4.addEventListener("click", fm);
+level5.addEventListener("click", fn, true);
+level5.addEventListener("click", fm);
+level6.addEventListener("click", fn, true);
+level6.addEventListener("click", fm);
+level7.addEventListener("click", fn, true);
+level7.addEventListener("click", fm);
 
 //# sourceMappingURL=index.fef1cfaf.js.map
